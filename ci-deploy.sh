0f6c10f32d860b62ca7aea30c40788e06b60f8d6
Renato Vasconcellos <revagomes@gmail.com>
2017-08-08 20:52:30 -0300

    Updated ci-deploy script to use nvm version to 7.9.0.

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
-npm install
+nvm install 7.9.0
+nvm use 7.9.0
 npm run build
 rsync -avzhe "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" --exclude '.git' --delete ./.tmp/ taller-docker@67.205.177.225:/mnt/volume-baleia-prod/taller-site/
