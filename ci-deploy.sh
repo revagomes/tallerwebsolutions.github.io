55d19bb4b13fab3221135598ddf1bdd0ec0c3d8e
Renato Vasconcellos <revagomes@gmail.com>
2017-08-08 20:13:38 -0300

    Updated ci-deploy script.

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
-npm use
 npm install
 npm run build
 rsync -avzhe "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" --exclude '.git' --delete ./.tmp/ taller-docker@67.205.177.225:/mnt/volume-baleia-prod/taller-site/
